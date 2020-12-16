e67d1c0f5a98ec483df0d06cc6fcb6d60cca9bff
Eric E. Gunes <mr.gunes@msn.com>
2020-12-15 22:46:17 -0500

    Using array methods, you will transform a string into an array!

---
+++ script.js
@@ -0,0 +1,5 @@
+//Transform a string into an array and fix it!
+
+let quote = "Some body can write code that a very well designed code machines can understand. Good programmers write code that humans can understand.” – Martin Fowler. Agile Software Design";
+
+
